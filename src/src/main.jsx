d9351a6f4bfbc12aa91e8d95361317ae83068c64
glitchaicoin <glitchaicoin@gmail.com>
2025-05-23 07:53:05 +0300

    Create main.jsx

---
+++ src/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import GLAILanding from './GLAILanding'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GLAILanding />
+  </React.StrictMode>
+)

39eba44201cf84ebf95e66d022c5a87f03245e50
glitchaicoin <glitchaicoin@gmail.com>
2025-05-23 10:24:57 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import GLAILanding from './GLAILanding'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import GLAILanding from './components/GLAILanding'; // ✅ make sure this path matches
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GLAILanding />
   </React.StrictMode>
-)
+);
